ed874a7c21f3e3c1338ba648846e73cdb57b87d6
Samus_ <mail2samus@gmail.com>
2010-12-06 04:29:31 -0200

    adding methods on models to retrieve Expando-properties

--- redditwatch/models.py
+++ redditwatch/models.py
@@ -6,6 +6,13 @@ class PingResultModel(db.Expando):
     status_code = db.IntegerProperty()
     content     = db.TextProperty()
 
+    def get_headers(self):
+        headers = {}
+        for attr_name in dir(self):
+            if attr_name.startswith('header-'):
+                headers[attr_name[7:]] = getattr(self, attr_name)
+        return headers
+
 
 class AverageResultModel(db.Expando):
     start    = db.DateTimeProperty()
@@ -15,4 +22,11 @@ class AverageResultModel(db.Expando):
     min_delay = db.FloatProperty()
     max_delay = db.FloatProperty()
     avg_delay = db.FloatProperty()
+
+    def get_status_counts(self):
+        status_counts = {}
+        for attr_name in dir(self):
+            if attr_name.startswith('status_'):
+                status_counts[attr_name[7:]] = getattr(self, attr_name)
+        return status_counts
 
